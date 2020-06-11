c48246e86ae9b017801b4deea4e1fa66f92e453f
Silas Hellinger <446547+Aevis@users.noreply.github.com>
2020-06-11 03:27:06 +0200

    Add a Docker Compose setup to run tests locally

--- tests/ci/db/post_install.sh
+++ tests/ci/db/post_install.sh
@@ -9,10 +9,12 @@ source "${INFORMIX_HOME}/.bashrc"
 source "/opt/ibm/scripts/informix_inf.env"
 
 # Create the sbspace
-echo ">>>    Creating sbspace"
-touch "${INFORMIX_DATA_DIR}"/spaces/sbspace
-chmod 660 "${INFORMIX_DATA_DIR}"/spaces/sbspace
-onspaces -c -S sbspace -p "${INFORMIX_DATA_DIR}"/spaces/sbspace -o 0 -s 20000
+if [ ! -f "${INFORMIX_DATA_DIR}"/spaces/sbspace ]; then
+  echo ">>>    Creating sbspace"
+  touch "${INFORMIX_DATA_DIR}"/spaces/sbspace
+  chmod 660 "${INFORMIX_DATA_DIR}"/spaces/sbspace
+  onspaces -c -S sbspace -p "${INFORMIX_DATA_DIR}"/spaces/sbspace -o 0 -s 20000
+fi
 
 # Create the test database
 echo ">>>    Creating test database"
